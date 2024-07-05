53d4b35553d7539842e39c75cc9358b0c49aad6d
Gordon Pettus <38599393+GLPUSNA67@users.noreply.github.com>
2024-07-04 17:45:31 -0700

    Working on save current and reload

---
+++ MS_Solution_1.py
@@ -0,0 +1,221 @@
+''' This solution format did not work. 
+See the latest version of MonsterSudoku.
+And MS_....txt files for the current solutions. '''
+print("3043 Entered load_solution_1")
+global currentNumber
+currentNumber = 'B'
+update_cell(btn_R1C2, arrRef1)
+currentNumber = '2'
+update_cell(btn_R1C3, arrRef2)
+currentNumber = '1'
+update_cell(btn_R1C4, arrRef3)
+currentNumber = '3'
+update_cell(btn_R1C8, arrRef7)
+currentNumber = '9'
+update_cell(btn_R1C11, arrRef10)
+currentNumber = '0'
+update_cell(btn_R1C15, arrRef14)
+currentNumber = '7'
+update_cell(btn_R1C16, arrRef15)
+currentNumber = 'F'
+update_cell(btn_R2C1, arrRef16)
+currentNumber = '5'
+update_cell(btn_R2C3, arrRef18)
+currentNumber = 'D'
+update_cell(btn_R2C5, arrRef20)
+currentNumber = '4'
+update_cell(btn_R2C11, arrRef26)
+currentNumber = 'B'
+update_cell(btn_R2C13, arrRef28)
+currentNumber = 'C'
+update_cell(btn_R2C14, arrRef29)
+currentNumber = 'B'
+update_cell(btn_R3C6, arrRef37)
+currentNumber = '6'
+update_cell(btn_R3C7, arrRef38)
+currentNumber = '5'
+update_cell(btn_R3C9, arrRef40)
+currentNumber = '1'
+update_cell(btn_R3C10, arrRef41)
+currentNumber = 'D'
+update_cell(btn_R3C14, arrRef45)
+currentNumber = '0'
+update_cell(btn_R4C3, arrRef50)
+currentNumber = '0'
+update_cell(btn_R4C3, arrRef50)
+currentNumber = '1'
+update_cell(btn_R4C7, arrRef54)
+currentNumber = '5'
+update_cell(btn_R4C8, arrRef55)
+currentNumber = 'C'
+update_cell(btn_R4C9, arrRef56)
+currentNumber = 'D'
+update_cell(btn_R4C10, arrRef57)
+currentNumber = 'F'
+update_cell(btn_R4C12, arrRef59)
+currentNumber = 'E'
+update_cell(btn_R4C14, arrRef61)
+currentNumber = 'A'
+update_cell(btn_R4C16, arrRef63)
+currentNumber = 'A'
+update_cell(btn_R5C5, arrRef68)
+currentNumber = 'D'
+update_cell(btn_R5C7, arrRef70)
+currentNumber = '4'
+update_cell(btn_R5C10, arrRef73)
+currentNumber = '3'
+update_cell(btn_R5C16, arrRef79)
+currentNumber = 'B'
+update_cell(btn_R6C1, arrRef80)
+currentNumber = '8'
+update_cell(btn_R6C2, arrRef81)
+currentNumber = 'F'
+update_cell(btn_R6C3, arrRef82)
+currentNumber = '4'
+update_cell(btn_R6C4, arrRef83)
+currentNumber = '6'
+update_cell(btn_R6C6, arrRef85)
+currentNumber = '9'
+update_cell(btn_R6C8, arrRef87)
+currentNumber = '2'
+update_cell(btn_R6C11, arrRef90)
+currentNumber = 'D'
+update_cell(btn_R6C13, arrRef92)
+currentNumber = '7'
+update_cell(btn_R6C14, arrRef93)
+currentNumber = 'E'
+update_cell(btn_R7C1, arrRef96)
+currentNumber = '9'
+update_cell(btn_R7C4, arrRef99)
+currentNumber = '6'
+update_cell(btn_R7C11, arrRef106)
+currentNumber = '0'
+update_cell(btn_R7C12, arrRef107)
+currentNumber = '1'
+update_cell(btn_R7C13, arrRef108)
+currentNumber = '2'
+update_cell(btn_R7C15, arrRef110)
+currentNumber = '0'
+update_cell(btn_R8C1, arrRef112)
+currentNumber = '6'
+update_cell(btn_R8C4, arrRef115)
+currentNumber = '7'
+update_cell(btn_R8C6, arrRef117)
+currentNumber = '5'
+update_cell(btn_R8C7, arrRef118)
+currentNumber = 'E'
+update_cell(btn_R8C10, arrRef121)
+currentNumber = 'A'
+update_cell(btn_R8C11, arrRef122)
+currentNumber = '4'
+update_cell(btn_R8C14, arrRef125)
+currentNumber = '8'
+update_cell(btn_R8C15, arrRef126)
+currentNumber = '6'
+update_cell(btn_R9C2, arrRef129)
+currentNumber = 'B'
+update_cell(btn_R9C3, arrRef130)
+currentNumber = '5'
+update_cell(btn_R9C6, arrRef133)
+currentNumber = 'C'
+update_cell(btn_R9C7, arrRef134)
+currentNumber = '2'
+update_cell(btn_R9C10, arrRef137)
+currentNumber = 'D'
+update_cell(btn_R9C11, arrRef138)
+currentNumber = '0'
+update_cell(btn_R9C13, arrRef140)
+currentNumber = 'F'
+update_cell(btn_R9C16, arrRef143)
+currentNumber = 'C'
+update_cell(btn_R10C2, arrRef145)
+currentNumber = 'F'
+update_cell(btn_R10C4, arrRef147)
+currentNumber = '9'
+update_cell(btn_R10C5, arrRef148)
+currentNumber = 'E'
+update_cell(btn_R10C6, arrRef149)
+currentNumber = '7'
+update_cell(btn_R10C13, arrRef156)
+currentNumber = 'B'
+update_cell(btn_R10C16, arrRef159)
+currentNumber = '4'
+update_cell(btn_R11C3, arrRef162)
+currentNumber = '2'
+update_cell(btn_R11C4, arrRef163)
+currentNumber = 'F'
+update_cell(btn_R11C6, arrRef165)
+currentNumber = '9'
+update_cell(btn_R11C9, arrRef168)
+currentNumber = 'C'
+update_cell(btn_R11C11, arrRef170)
+currentNumber = '3'
+update_cell(btn_R11C13, arrRef172)
+currentNumber = 'A'
+update_cell(btn_R11C14, arrRef173)
+currentNumber = 'E'
+update_cell(btn_R11C15, arrRef174)
+currentNumber = 'D'
+update_cell(btn_R11C16, arrRef175)
+currentNumber = '5'
+update_cell(btn_R12C1, arrRef176)
+currentNumber = '7'
+update_cell(btn_R12C7, arrRef182)
+currentNumber = 'F'
+update_cell(btn_R12C10, arrRef185)
+currentNumber = '6'
+update_cell(btn_R12C12, arrRef187)
+currentNumber = '2'
+update_cell(btn_R13C1, arrRef192)
+currentNumber = 'E'
+update_cell(btn_R13C3, arrRef194)
+currentNumber = '6'
+update_cell(btn_R13C5, arrRef196)
+currentNumber = '0'
+update_cell(btn_R13C7, arrRef198)
+currentNumber = '8'
+update_cell(btn_R13C8, arrRef199)
+currentNumber = '4'
+update_cell(btn_R13C9, arrRef200)
+currentNumber = 'B'
+update_cell(btn_R13C10, arrRef201)
+currentNumber = '1'
+update_cell(btn_R13C14, arrRef205)
+currentNumber = '9'
+update_cell(btn_R14C3, arrRef210)
+currentNumber = 'F'
+update_cell(btn_R14C7, arrRef214)
+currentNumber = 'E'
+update_cell(btn_R14C8, arrRef215)
+currentNumber = '8'
+update_cell(btn_R14C10, arrRef217)
+currentNumber = '7'
+update_cell(btn_R14C11, arrRef218)
+currentNumber = '8'
+update_cell(btn_R15C3, arrRef226)
+currentNumber = 'B'
+update_cell(btn_R15C4, arrRef227)
+currentNumber = '4'
+update_cell(btn_R15C6, arrRef229)
+currentNumber = '2'
+update_cell(btn_R15C12, arrRef235)
+currentNumber = '0'
+update_cell(btn_R15C14, arrRef237)
+currentNumber = 'E'
+update_cell(btn_R15C16, arrRef239)
+currentNumber = 'C'
+update_cell(btn_R16C1, arrRef240)
+currentNumber = '7'
+update_cell(btn_R16C2, arrRef241)
+currentNumber = '1'
+update_cell(btn_R16C6, arrRef245)
+currentNumber = '0'
+update_cell(btn_R16C9, arrRef248)
+currentNumber = '8'
+update_cell(btn_R16C13, arrRef252)
+currentNumber = '2'
+update_cell(btn_R16C14, arrRef253)
+currentNumber = 'B'
+update_cell(btn_R16C15, arrRef254)
+cells_done()
+cells_remaining()
---
+++ MS_Solutions.py
@@ -0,0 +1,70 @@
+''' This solution format did not work. 
+See the latest version of MonsterSudoku.
+And MS_....txt files for the current solutions. '''
+print("3043 Entered load_solution_1")
+# import MS_Solutions_data as solution
+solution = [["F", 1, 2, 1], ["A", 1, 5, 2], ["9", 8, 9, 8]]
+solutionText = ""
+entryNums = 0
+for entry in solution:
+    # print(entry)
+    num = solution[entryNums][0]
+    row = solution[entryNums][1]
+    col = solution[entryNums][2]
+    sq = solution[entryNums][3]
+    btn = f"btnR{row}C{col}"
+    print("currentNumber = ", num)
+    print("btn is ", btn)
+    solutionText = solutionText + f"currentNumber = {num}\nupdateCell(btnR{row}C{col})\n"
+    entryNums += 1
+
+print("Number of entries is ", entryNums)
+print("19 solutionText is:")
+print(solutionText)
+''' I couldn't get file reading and parsing to work.'''
+solution = []
+# print("22 ", solution)
+file = open("MS_Solutions.txt", "r")
+# To parse a solution from a file, do the below and convert the output to 
+# puzzle update commands. 
+# The following will parse the first list into num, row, col, and sq
+# this can be duplicated up through RCS = 9, but above that, the index 
+# needs to be changes from -1 to -2
+print("30")
+for line in file:
+    # for item in line:
+    # solution = solution +  line
+    print(line)
+    # # l = solution
+    # l = solution[1:-1]
+    
+    # print("31 ", l)
+    # # print("32 ", new_l)
+    # text = l[1:-1]
+    # # results = l.split(',')
+    # # print("35 results are ", results)
+    # i = text.index(',')
+    # # print("40 index i is ", i)
+    # num = text[i -2]
+    # print("42 num is ", num)
+    # text = text[i+1:]
+    # i = text.index(',')
+    # # print("44 index i is ", i)
+    # row = text[i-1]
+    # # print("47 text is ", text)
+    # print("48 row is ", row)
+    # text = text[i+1:]
+    # i = text.index(',')
+    # col = text[i -1]
+    # text = text[i+1:]
+    # i = text.index(',')
+    # sq = text[i -2]
+    # print("55 sq is ", sq)
+    # print("36 text is ", text)
+    # print("text[0:]", text[0:])
+    # print("text[1:]", text[1:])
+    # print("text[2:]", text[2:])
+    # print("text[1:][1:2]", text[1:][0:1])
+    # print(text[1:][4:5])
+    # print(text[1:][8:9])
+    # print(text[1:][11:12])
